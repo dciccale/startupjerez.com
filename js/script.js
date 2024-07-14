ec1269aec7f8c68542dd3bafa9bd26a3b0eec64a
dciccale <denis@Deniss-MBP-3.home>
2024-07-14 17:16:25 +0200

    fix mobile menu

--- js/script.js
+++ js/script.js
@@ -42,3 +42,14 @@ window.addEventListener('load', () => {
   document.querySelector('#language').value = savedLanguage;
   changeLanguage(savedLanguage);
 });
+
+
+// Open mobile menu
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('menu-toggle');
+    const menu = document.getElementById('mobile-menu-2');
+
+    toggleButton.addEventListener('click', function() {
+        menu.classList.toggle('hidden');
+    });
+});
