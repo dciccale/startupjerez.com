cb2fc41b2bfe9037a7235f20f5dcfb6dd85d96d9
Eugenio Ciccale <eugenio.ciccale@gmail.com>
2024-07-16 09:42:19 +0200

    fix mobile menu, theme, etc

---
+++ js/mobile-menu.js
@@ -0,0 +1,9 @@
+// Open mobile menu
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleButton = document.getElementById('menu-toggle');
+  const menu = document.getElementById('mobile-menu-2');
+
+  toggleButton.addEventListener('click', function () {
+    menu.classList.toggle('hidden');
+  });
+});
