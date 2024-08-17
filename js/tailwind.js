47a696752582212b673273cf2e363a93dd0892a9
dciccale <539546+dciccale@users.noreply.github.com>
2024-08-17 17:26:19 +0200

    fixes

--- js/tailwind.js
+++ js/tailwind.js
@@ -1,13 +1,26 @@
 tailwind.config = {
   darkMode: "class",
   colors: {
-    'yellow': '#FF940F',
+    yellow: "#FF940F",
   },
   theme: {
     extend: {
       colors: {
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
-      }
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+          950: "#172554",
+        },
+      },
     },
-  }
-}
+  },
+};
+
--- js/theme.js
+++ js/theme.js
@@ -1,26 +1,31 @@
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.getElementById('dark-icon').removeAttribute('hidden');
-  document.getElementById('light-icon').setAttribute('hidden', true);
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.getElementById("dark-icon").removeAttribute("hidden");
+  document.getElementById("light-icon").setAttribute("hidden", true);
 } else {
-  document.getElementById('light-icon').removeAttribute('hidden');
-  document.getElementById('dark-icon').setAttribute('hidden', true);
+  document.getElementById("light-icon").removeAttribute("hidden");
+  document.getElementById("dark-icon").setAttribute("hidden", true);
 }
 
 // Function to toggle the theme
 function toggleTheme() {
-  if (document.documentElement.classList.contains('dark')) {
-    document.documentElement.classList.remove('dark');
-    document.getElementById('dark-icon').setAttribute('hidden', true);
-    document.getElementById('light-icon').removeAttribute('hidden');
-    localStorage.theme = 'light';
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    document.getElementById("dark-icon").setAttribute("hidden", true);
+    document.getElementById("light-icon").removeAttribute("hidden");
+    localStorage.theme = "light";
   } else {
-    document.documentElement.classList.add('dark');
-    document.getElementById('light-icon').setAttribute('hidden', true);
-    document.getElementById('dark-icon').removeAttribute('hidden');
-    localStorage.theme = 'dark';
+    document.documentElement.classList.add("dark");
+    document.getElementById("light-icon").setAttribute("hidden", true);
+    document.getElementById("dark-icon").removeAttribute("hidden");
+    localStorage.theme = "dark";
   }
 }
 
 // Add event listener to the button
-document.getElementById('toggle-theme').addEventListener('click', toggleTheme);
+document.getElementById("toggle-theme").addEventListener("click", toggleTheme);
+
